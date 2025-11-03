7a3c07b14f6deca47fff366eacc77772c8530af4
lukmanxpert <lukmanmiah2004@gmail.com>
2025-11-03 15:34:38 +0600

    lookup table using reduce

---
+++ MISSION 01 [BE A CRITICAL THINKER & TYPESCRIPT TECHNOCRAT]/MODULE 02/M02V06.js
@@ -0,0 +1,36 @@
+const postsData = [
+  {
+    postId: 1,
+    title: "Understanding JavaScript Closures",
+    author: "Arafat",
+  },
+  {
+    postId: 2,
+    title: "React vs Vue: Which One to Choose?",
+    author: "Mira",
+  },
+  {
+    postId: 3,
+    title: "Mastering MongoDB Aggregation",
+    author: "Siam",
+  },
+  {
+    postId: 4,
+    title: "Node.js Best Security Practices",
+    author: "Hasib",
+  },
+  {
+    postId: 5,
+    title: "How to Build REST APIs with Express",
+    author: "Tisha",
+  },
+];
+
+const lookUpTable = postsData.reduce((table, post) => {
+  table[post.postId] = post;
+  return table;
+}, {});
+console.log(postsData);
+console.log("lookUpTable :>> ", lookUpTable);
+
+const post = lookUpTable["1"];
