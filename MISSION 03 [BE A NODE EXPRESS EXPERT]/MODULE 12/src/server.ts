6e9f651bb4a1c486d77ea5d90d353ef54dd16e69
lukmanxpert <lukmanmiah2004@gmail.com>
2025-11-30 19:09:01 +0600

    express json middleware and make a connection via pg

--- MISSION 03 [BE A NODE EXPRESS EXPERT]/MODULE 12/src/server.ts
+++ MISSION 03 [BE A NODE EXPRESS EXPERT]/MODULE 12/src/server.ts
@@ -1,9 +1,27 @@
 import express, { Request, Response } from "express";
+import { Pool } from "pg";
+
 const app = express();
 const port = 5000;
 
+const pool = new Pool({
+  connectionString:
+    "postgresql://neondb_owner:npg_laYCSfw75NJU@ep-aged-hat-adjigyie-pooler.c-2.us-east-1.aws.neon.tech/neondb?sslmode=require&channel_binding=require",
+});
+
+// middleware
+app.use(express.json());
+
 app.get("/", (req: Request, res: Response) => {
-  res.send("Hello");
+  res.send("Hello world...");
+});
+
+app.post("/", (req: Request, res: Response) => {
+  console.log(req.body);
+  return res.status(201).json({
+    message: "Api is working fine",
+    data: req.body,
+  });
 });
 
 app.listen(port, () => {
