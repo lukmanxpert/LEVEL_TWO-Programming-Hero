900984f80bf779720a5145635d1f87471e208fea
lukmanxpert <lukmanmiah2004@gmail.com>
2025-11-30 19:34:38 +0600

    write queries for create table for users

--- MISSION 03 [BE A NODE EXPRESS EXPERT]/MODULE 12/src/server.ts
+++ MISSION 03 [BE A NODE EXPRESS EXPERT]/MODULE 12/src/server.ts
@@ -4,13 +4,31 @@ import { Pool } from "pg";
 const app = express();
 const port = 5000;
 
+// middleware
+app.use(express.json());
+
+// db
 const pool = new Pool({
   connectionString:
     "postgresql://neondb_owner:npg_laYCSfw75NJU@ep-aged-hat-adjigyie-pooler.c-2.us-east-1.aws.neon.tech/neondb?sslmode=require&channel_binding=require",
 });
 
-// middleware
-app.use(express.json());
+const initDB = async () => {
+  await pool.query(`
+    CREATE TABLE IF NOT EXISTS users(
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(100) NOT NULL,
+    email VARCHAR(100) UNIQUE NOT NULL,
+    age INT,
+    phone VARCHAR(15),
+    address TEXT,
+    created_at TIMESTAMP DEFAULT NOW(),
+    updated_at TIMESTAMP DEFAULT NOW(),
+    )
+    `);
+};
+
+initDB();
 
 app.get("/", (req: Request, res: Response) => {
   res.send("Hello world...");
