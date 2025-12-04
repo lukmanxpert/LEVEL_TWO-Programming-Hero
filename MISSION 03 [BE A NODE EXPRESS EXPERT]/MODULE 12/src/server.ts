72014b4b8d691dcc285a998fb2e703bd931d6cf9
lukmanxpert <lukmanmiah2004@gmail.com>
2025-12-05 02:12:58 +0600

    config project structure

--- MISSION 03 [BE A NODE EXPRESS EXPERT]/MODULE 12/src/server.ts
+++ MISSION 03 [BE A NODE EXPRESS EXPERT]/MODULE 12/src/server.ts
@@ -1,46 +1,14 @@
 import express, { Request, Response } from "express";
-import { Pool } from "pg";
-import dotenv from "dotenv";
+import config from "./config";
+import initDB, { pool } from "./config/db";
 
 const app = express();
-const port = 5000;
+const port = config.port;
 
 // middleware
 app.use(express.json());
-dotenv.config();
 
-// db
-const pool = new Pool({
-  connectionString: process.env.CONNECTION_STRING_PG,
-});
-
-const initDB = async () => {
-  await pool.query(`
-    CREATE TABLE IF NOT EXISTS users(
-    id SERIAL PRIMARY KEY,
-    name VARCHAR(100) NOT NULL,
-    email VARCHAR(100) UNIQUE NOT NULL,
-    age INT,
-    phone VARCHAR(15),
-    address TEXT,
-    created_at TIMESTAMP DEFAULT NOW(),
-    updated_at TIMESTAMP DEFAULT NOW()
-    )
-    `);
-  await pool.query(`
-    CREATE TABLE IF NOT EXISTS todos(
-    id SERIAL PRIMARY KEY,
-    user_id INT REFERENCES users(id) ON DELETE CASCADE,
-    title VARCHAR(200) NOT NULL,
-    description TEXT,
-    completed BOOLEAN DEFAULT false,
-    due_date DATE,
-    created_at TIMESTAMP DEFAULT NOW(),
-    updated_at TIMESTAMP DEFAULT NOW()
-    )
-    `);
-};
-
+// initializing database
 initDB();
 
 app.get("/", (req: Request, res: Response) => {
